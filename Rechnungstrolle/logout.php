05253896585d714c0cb278ea410e57415ab561b5
ok <oliwersukic@gmail.com>
2017-03-17 12:23:10 +0100

    Rechnungswesen Gelöscht

--- Rechnungstrolle/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_destroy();
- 
-echo '<script language="javascript">
-		alert("Logout erfolgreich!")
-	  </script>
-	  <meta http-equiv="refresh" content="0; URL=login.php">';
-?>
